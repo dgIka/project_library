ab1969fd719869c4e9a0767783fb3e9541a4c281
Ika <rexar.ika@gmail.com>
2025-05-03 01:21:31 +0300

    library start empty

---
+++ library/src/main/java/ika/library/config/MySpringMvcDispatcherServletInitilizer.java
@@ -0,0 +1,38 @@
+package ika.library.config;
+
+import jakarta.servlet.FilterRegistration;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitilizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext context) {
+        FilterRegistration.Dynamic hiddenHttpMethodFilter = context.addFilter(
+                "hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()
+        );
+        hiddenHttpMethodFilter.addMappingForUrlPatterns(null, true, "/*");
+    }
+}
