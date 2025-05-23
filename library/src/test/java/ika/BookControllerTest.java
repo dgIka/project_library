a0dc9e563338234f10f2df351bbdf15dd606eeb4
Ika <rexar.ika@gmail.com>
2025-05-23 23:37:52 +0300

    junit practice, second controller test

--- library/src/test/java/ika/BookControllerTest.java
+++ library/src/test/java/ika/BookControllerTest.java
@@ -2,16 +2,21 @@ package ika;
 
 import ika.library.controllers.BookController;
 import ika.library.dao.BookDAO;
+import ika.library.dao.PersonDAO;
 import ika.library.models.Book;
+import ika.library.models.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.ui.Model;
 
+import java.time.LocalDate;
 import java.util.*;
 
 import static junit.framework.Assert.assertEquals;
 
 
 public class BookControllerTest {
+    private final SimpleModel simpleModel = new SimpleModel();
+
     @Test
     void index_ShouldReturnViewAndAddBooksToModel() {
         BookDAO mockDAO = new BookDAO(null) {
@@ -21,7 +26,6 @@ public class BookControllerTest {
             }
         };
         BookController bookController = new BookController(mockDAO, null);
-        SimpleModel simpleModel = new SimpleModel();
 
         String viewname = bookController.index(simpleModel);
 
@@ -31,5 +35,30 @@ public class BookControllerTest {
         assertEquals("1984", books.get(0).getTitle());
 
 
+    }
+     @Test
+    void show_ShouldAddTwoAttributesAndReturnView() {
+        BookDAO mockDAO = new BookDAO(null) {
+            @Override
+            public Book show(int id) {
+                return new Book("1984", "Оруэлл", 1949);
+            }
+        };
+        PersonDAO mockPersonDAO = new PersonDAO(null) {
+            @Override
+            public List<Person> index() {
+                return Collections.singletonList(new Person("Tom", LocalDate.of(1950, 1, 1)));
+            }
+        };
+        BookController bookController = new BookController(mockDAO, mockPersonDAO);
+
+        String viewname = bookController.show(0, simpleModel);
+
+        assertEquals("books/show", viewname);
+        Book book = (Book) simpleModel.getAttribute("book");
+       List<Person> persons = (List<Person>) simpleModel.getAttribute("people");
+        assertEquals("1984", book.getTitle());
+        assertEquals(1, persons.size());
+        assertEquals("Tom", persons.get(0).getName());
     }
 }
